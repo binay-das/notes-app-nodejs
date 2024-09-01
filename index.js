c68f575be83f7410824d03c6fb5beff8448b37fd
Binay <binaydas1123@gmail.com>
2024-09-02 00:44:54 +0530

    Few changes

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+let allNotes = [
+    {
+        content: "Hello guys",
+    },
+];
 app.get("/", (req, res)=>{
     res.render("home");
+})
+
+app.get("/allNotes",(req,res)=>{
+    console.log(allNotes);
+    res.render("allNotes", {allNotes});
 })
 
 const port = 8080;
