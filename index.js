ee3b8896543251f9b9257aa816b9317d3742cd9c
Binay <binaydas1123@gmail.com>
2024-09-03 01:30:17 +0530

    New routes setup

--- index.js
+++ index.js
@@ -5,22 +5,34 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-let allNotes = [
+let notes = [
     {
         content: "Hello guys",
     },
 ];
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render("home");
 })
 
-app.get("/allNotes",(req,res)=>{
-    console.log(allNotes);
-    res.render("allNotes", {allNotes});
+app.get("/notes", (req, res) => {
+    console.log(notes);
+    res.render("notes", { notes });
+})
+
+app.get("/notes/add", (req, res) => {
+    res.render("add");
+})
+
+app.post("/notes", (req, res) => {
+    let { content } = req.body;
+    console.log(req.body);
+    notes.push({ content });
+    res.redirect("/notes");
 })
 
 const port = 8080;
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Listening to port ${port}...`)
 })
